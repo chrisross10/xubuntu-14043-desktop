f7efe0327e155bc05c14f32946bcd841198a9a8f
chris <chris.ross@7digital.com>
2015-11-25 18:00:48 +0000

    Update bootstrap and remove contributers.txt

--- Vagrantfile
+++ Vagrantfile
@@ -14,6 +14,10 @@ Vagrant.configure(2) do |config|
   # boxes at https://atlas.hashicorp.com/search.
   config.vm.box = "neil_kidd/xubuntu-14043-desktop"
 
+  # Enable the vm to use the hosts ssh agent enabling the ability to clone
+  # private repositories using the hosts credentials
+  config.ssh.forward_agent = true
+
   # Disable automatic box update checking. If you disable this, then
   # boxes will only be checked for updates when the user runs
   # `vagrant box outdated`. This is not recommended.
--- bootstrap.sh
+++ bootstrap.sh
@@ -7,10 +7,10 @@ function install {
     apt-get -y install "$@" >/dev/null 2>&1
 }
 
-echo Updating package information...
-sudo apt-get -y update
+echo "<<<<<<< UPDATING PACKAGE INFORMATION >>>>>>>"
+sudo apt-get -y update >/dev/null 2>&1
 
-echo Installing and configuring Git...
+echo "<<<<<<< INSTALLING AND CONFIGURING GIT >>>>>>>"
 sudo apt-get -y install git
 #git config --global user.name "Name Surname"
 #git config --global user.email nick@company.com
@@ -20,35 +20,32 @@ sudo apt-get -y install git
 #git config --global color.diff auto
 #git config --global color.status auto
 
-echo Installing Ruby with rbenv...
-git clone git://github.com/sstephenson/rbenv.git .rbenv
+echo "<<<<<<< INSTALLING RUBY WITH RBENV >>>>>>>"
+git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-
 git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
-
 git clone https://github.com/ianheggie/rbenv-binstubs.git ~/.rbenv/plugins/rbenv-binstubs
 bundle install --binstubs .bundle/bin
 
-echo Installing and setting global Ruby version...
+echo "<<<<<<< INSTALLING AND SETTING GLOBAL RUBY VERSION >>>>>>>"
 # list all available versions:
-#rbenv install -l
+rbenv install -l
 # install a Ruby version:
 rbenv install 2.2.3
 # choose default ruby version on your system
 rbenv global 2.2.3
 ruby -v
 
-echo Installing Rails...
+echo "<<<<<<< INSTALLING RAILS >>>>>>>"
 sudo apt-get -y install rails
 sudo gem install rails
 
 echo "<<<<<<< INSTALLING CHROMIUM >>>>>>>"
 sudo apt-get -y install chromium-browser
-echo Install Atom...
+echo "<<<<<<< INSTALL ATOM >>>>>>>"
 sudo dpkg --install /vagrant/assets/atom/atom-amd64.deb
 echo "<<<<<<< INSTALLING ATOM PACKAGES >>>>>>>"
 apm install --packages-file /vagrant/assets/atom/packages.txt
